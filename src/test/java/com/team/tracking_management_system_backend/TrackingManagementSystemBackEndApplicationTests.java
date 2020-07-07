9d0c3099019d0c2fada3d17499d3055b187bcb81
ForU <18644045901@163.com>
2020-07-07 19:18:54 +0800

    init commit

---
+++ src/test/java/com/team/tracking_management_system_backend/TrackingManagementSystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.tracking_management_system_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingManagementSystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
